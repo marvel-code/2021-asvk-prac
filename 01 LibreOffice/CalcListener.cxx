52d07e1e53ba17f4e9050e999a327620d76e0f7c
Rodion Kostin <itrodion@gmail.com>
2021-09-28 14:31:23 +0300

    fixup

--- 01 LibreOffice/CalcListener.cxx
+++ 01 LibreOffice/CalcListener.cxx
@@ -2,5 +2,12 @@
 #include "MyListener.h"
 #include <com/sun/star/lang/XMultiServiceFactory.hpp>
 
-void SAL_CALL CalcListener::notifyEvent(const ::com::sun::star::document::EventObject &aEvent) {}
-void SAL_CALL CalcListener::disposing(const com::sun::star::lang::EventObject &aSource) {}
+void SAL_CALL 
+CalcListener::notifyEvent(
+    const ::com::sun::star::document::EventObject &aEvent
+) {}
+
+void SAL_CALL 
+CalcListener::disposing(
+    const com::sun::star::lang::EventObject &aSource
+) {}
