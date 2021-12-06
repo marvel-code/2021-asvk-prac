a4a0d24b1cb047aac5efc1fa66f208dcfc43ee83
Rodion Kostin <itrodion@gmail.com>
2021-12-06 21:45:02 +0300

    02: Introduce task #2

---
+++ 02/GraphGenerator/src/generator.cpp
@@ -0,0 +1,5 @@
+#include "generator.h"
+
+const char* some() {
+    return "Hello world!!!!";
+}
---
+++ 02/GraphGenerator/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "generator.h"
+
+int main() {
+    std::cout << some() << std::endl;
+}
