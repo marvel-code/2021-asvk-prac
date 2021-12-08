070a594089aa68ae44ce6d8643c93a90f069830b
Rodion Kostin <itrodion@gmail.com>
2021-12-08 14:10:56 +0300

    03: BipartiteGraph: Implement constructor

--- 03 TGraph/src/BipartiteGraph.cpp
+++ 03 TGraph/src/BipartiteGraph.cpp
@@ -1,2 +1,24 @@
 #include "h/BipartiteGraph.h"
+#include "h/graphUtils.h"
 
+BipartiteGraph::BipartiteGraph(
+    std::vector<char> topVertices, 
+    std::vector<char> bottomVertices
+) : _topVertices(topVertices), _bottomVertices(bottomVertices) {
+
+    // Fill vertices
+    _vertices.reserve(topVertices.size() + bottomVertices.size());
+    for (auto vertex: topVertices) {
+        _vertices.push_back(vertex);
+    }
+    for (auto vertex: bottomVertices) {
+        _vertices.push_back(vertex);
+    }
+
+    // Make edges
+    for (auto topVertex: topVertices) {
+        for (auto bottomVertex: bottomVertices) {
+            _edges.push_back(std::vector<char>{ topVertex, bottomVertex });
+        }
+    }
+}
--- 03 TGraph/src/h/BipartiteGraph.h
+++ 03 TGraph/src/h/BipartiteGraph.h
@@ -1,5 +1,17 @@
 #include "TGraph.h"
 
 class BipartiteGraph : public TGraph {
-    
+
+private:
+    std::vector<char> _topVertices;
+    std::vector<char> _bottomVertices;
+
+public:
+    BipartiteGraph() = default;
+    BipartiteGraph(std::vector<char> topVertices, std::vector<char> bottomVertices);
+
+    std::string ToString() const override;
+
+    friend BipartiteGraph operator+(const BipartiteGraph& g1, const BipartiteGraph& g2);
+    friend BipartiteGraph operator-(const BipartiteGraph& g1, const BipartiteGraph& g2);
 };
