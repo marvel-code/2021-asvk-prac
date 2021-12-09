54626811377b6e058b7490880f17207b76a24530
Rodion Kostin <itrodion@gmail.com>
2021-12-10 00:06:45 +0300

    03: Fix Dijstra shortest path implementation

--- 03 TGraph/src/graphUtils.cpp
+++ 03 TGraph/src/graphUtils.cpp
@@ -5,7 +5,15 @@
 #include "h/WeightedGraph.h"
 #include <limits.h>
 
+#include <iostream>
+using std::cout, std::endl, std::cin;
+
 using std::string, std::vector;
+
+bool equalUnorderedEdges(Edge e1, Edge e2) {
+  return e1[0] == e2[0] && e1[1] == e2[1]
+        || e1[0] == e2[1] && e1[1] == e2[0];
+}
 
 string makeEdgeString(const Edge& edge) {
     string result;
@@ -44,16 +52,19 @@ vector<Edge> makeDijkstraShortestPath(
     const Vertex& startVertex,  
     const Vertex& endVertex
 ) {
+    // Case: same vertex
+    if (startVertex == endVertex && findIndex(graph.GetVertices(), startVertex) != -1)
+      return vector<Edge>();
+    // Exception: negative weight
     auto weights = graph.GetWeights();
     if (findIndex(weights, std::function([](int weight) { return weight < 0; })) != -1)
         throw std::invalid_argument("Failed to make Dijkstra shortest path: graph must not have negative weight.");
 
+    // Init
     auto vertices = graph.GetVertices();
     auto edges = graph.GetEdges();
     int startVertexIndex = findIndex(vertices, startVertex);
     int endVertexIndex = findIndex(vertices, endVertex);
-
-    // Init vertex informations: visited and minimum total weight from start.
     using VertexInfo = std::pair<bool/*visited*/, int/*min total weight from start*/>;
     vector<VertexInfo> vertexInfos(vertices.size(), std::pair<bool, int>(false, INT_MAX));
 
@@ -70,12 +81,22 @@ vector<Edge> makeDijkstraShortestPath(
       graphMatrix[v2Index][v1Index] = weight;
     }
     
+    // // Printing Graph matrix
+    // for (int i = 0; i < vertices.size(); ++i) 
+    //   cout << vertices[i] << ' ';
+    // cout << endl;
+    // for (auto y: graphMatrix) {
+    //   for (auto x: y) {
+    //     cout << x << " ";
+    //   }
+    //   cout << endl;
+    // }
+    
     // Calc total weights to vertices
     int visitedVertexCount = 0;
     Vertex vertex = startVertex;
     int vertexIndex = startVertexIndex;
-    VertexInfo vertexInfo = vertexInfos[vertexIndex];
-    vertexInfo.second = 0;
+    vertexInfos[vertexIndex].second = 0;
     do {
       // Stage 1. Update near vertices.
       auto weightToNearVertices = graphMatrix[vertexIndex];
@@ -83,40 +104,43 @@ vector<Edge> makeDijkstraShortestPath(
         int weightToNearVertex = weightToNearVertices[i];
         if (weightToNearVertex == -1)
           continue;
-        int totalWeightToNearVertex = weightToNearVertex + vertexInfo.second;
+        int totalWeightToNearVertex = weightToNearVertex + vertexInfos[vertexIndex].second;
         if (totalWeightToNearVertex < vertexInfos[i].second)
           vertexInfos[i].second = totalWeightToNearVertex;
       }
-      vertexInfo.first = true;
+      vertexInfos[vertexIndex].first = true;
       ++visitedVertexCount;
       if (visitedVertexCount == vertices.size())
         break;
 
       // Stage 2. Hop next vertex.
-      int minTotalWeightedVertexIndex = -1;
+      int nextVertexIndex = -1;
       for (int i = 0; i < vertices.size(); ++i) {
         if (weightToNearVertices[i] == -1 || vertexInfos[i].first)
           continue; 
-        if (vertexInfos[i].second < vertexInfos[minTotalWeightedVertexIndex].second)
-          minTotalWeightedVertexIndex = i;
+        if (nextVertexIndex == -1 || vertexInfos[i].second < vertexInfos[nextVertexIndex].second)
+          nextVertexIndex = i;
       }
       // Check for graph connectivity.
-      if (minTotalWeightedVertexIndex == -1) {
+      if (nextVertexIndex == -1) {
         if (!vertexInfos[endVertexIndex].first)
           throw std::logic_error(std::string("No path from ").append(1, startVertex).append(" to ").append(1, endVertex).append("."));
         break;
       }
       // Hop
-      vertexIndex = minTotalWeightedVertexIndex;
+      vertexIndex = nextVertexIndex;
       vertex = vertices[vertexIndex];
-      vertexInfo = vertexInfos[vertexIndex];
     } while (true);
+
+    // // Print vertex infos
+    // for (int i = 0; i < vertices.size(); ++i) {
+    //   cout << vertices[i] << " " << vertexInfos[i].first << " " << vertexInfos[i].second << endl;
+    // }
 
     // Make shortest path from startVertex to endVertex
     vector<Edge> shortestPath;
     vertex = endVertex;
     vertexIndex = endVertexIndex;
-    vertexInfo = vertexInfos[vertexIndex];
     do {
       // Find next hop
       int nextVertexIndex = -1;
@@ -125,17 +149,20 @@ vector<Edge> makeDijkstraShortestPath(
         int weightToNearVertex = weightToNearVertices[i];
         if (weightToNearVertex == -1)
           continue;
-        if (vertexInfo.second == weightToNearVertex + vertexInfos[i].second)
+        if (vertexInfos[vertexIndex].second == weightToNearVertex + vertexInfos[i].second) {
           nextVertexIndex = i;
+          break;
+        }
       }
       if (nextVertexIndex == -1)
         throw std::runtime_error("Not found next hop on shortest path make.");
       // Add edge
-      shortestPath.push_back(Edge{ vertex, vertices[vertexIndex] });
+      Edge edge{ vertex, vertices[nextVertexIndex] };
+      int edgeIndex = findIndex(edges, std::function([&edge](Edge e){ return equalUnorderedEdges(e, edge); }));
+      shortestPath.push_back(edges[edgeIndex]);
       // Hop
       vertexIndex = nextVertexIndex;
       vertex = vertices[vertexIndex];
-      vertexInfo = vertexInfos[vertexIndex];
     } while (vertex != startVertex);
 
     return std::vector(shortestPath.rbegin(), shortestPath.rend());
--- 03 TGraph/src/main.cpp
+++ 03 TGraph/src/main.cpp
@@ -5,36 +5,42 @@
 #include "h/WeightedGraph.h"
 #include "h/BipartiteGraph.h"
 #include <vector>
+#include "h/graphUtils.h"
 
 using std::cout, std::cin, std::endl;
 
 int main(int argc, char** argv) {
     GraphFactory graphFactory;
 
-    auto cg1 = (CompleteGraph*)graphFactory.Create("complete", { 'A', 'B', 'C', 'D' });
-    auto cg2 = (CompleteGraph*)graphFactory.Create("complete", { 'A', 'B', 'C' });
-    cout << (*cg1 + *cg2).ToString() << endl;
-    cout << (*cg1 - *cg2).ToString() << endl;
+    // auto cg1 = (CompleteGraph*)graphFactory.Create("complete", { 'A', 'B', 'C', 'D' });
+    // auto cg2 = (CompleteGraph*)graphFactory.Create("complete", { 'A', 'B', 'C' });
+    // cout << (*cg1 + *cg2).ToString() << endl;
+    // cout << (*cg1 - *cg2).ToString() << endl;
     
-    auto sg1 = (SimpleGraph*)graphFactory.Create("simple", { "AB", "DE", "BC" });
-    auto sg2 = (SimpleGraph*)graphFactory.Create("simple", { "BC", "CD" });
-    cout << (*sg1 + *sg2).ToString() << endl;
-    cout << (*sg1 - *sg2).ToString() << endl;
+    // auto sg1 = (SimpleGraph*)graphFactory.Create("simple", { "AB", "DE", "BC" });
+    // auto sg2 = (SimpleGraph*)graphFactory.Create("simple", { "BC", "CD" });
+    // cout << (*sg1 + *sg2).ToString() << endl;
+    // cout << (*sg1 - *sg2).ToString() << endl;
 
-    auto bg1 = (BipartiteGraph*)graphFactory.Create("bipartite", { 'A', 'B' }, { 'C', 'D', 'E' });
-    auto bg2 = (BipartiteGraph*)graphFactory.Create("bipartite", { 'A', 'B' }, { 'D', 'E' });
-    cout << (*bg1 + *bg2).ToString() << endl;
-    cout << (*bg1 - *bg2).ToString() << endl;
+    // auto bg1 = (BipartiteGraph*)graphFactory.Create("bipartite", { 'A', 'B' }, { 'C', 'D', 'E' });
+    // auto bg2 = (BipartiteGraph*)graphFactory.Create("bipartite", { 'A', 'B' }, { 'D', 'E' });
+    // cout << (*bg1 + *bg2).ToString() << endl;
+    // cout << (*bg1 - *bg2).ToString() << endl;
     
-    auto wg1 = (WeightedGraph*)graphFactory.Create("weighted", { "AB", "BC", "EA" }, { 1, 2, 3 });
-    auto wg2 = (WeightedGraph*)graphFactory.Create("weighted", { "BC", "CD", "AB" }, { 1, 5, 3 });
-    cout << (*wg1 + *wg2).ToString() << endl;
-    cout << (*wg1 - *wg2).ToString() << endl;
-    cout << (*wg1 - *sg1).ToString() << endl;
-    cout << (*sg1 - *wg1).ToString() << endl;
+    // auto wg1 = (WeightedGraph*)graphFactory.Create("weighted", { "AB", "BC", "EA" }, { 1, 2, 3 });
+    // auto wg2 = (WeightedGraph*)graphFactory.Create("weighted", { "BC", "CD", "AB" }, { 1, 5, 3 });
+    // cout << (*wg1 + *wg2).ToString() << endl;
+    // cout << (*wg1 - *wg2).ToString() << endl;
+    // cout << (*wg1 - *sg1).ToString() << endl;
+    // cout << (*sg1 - *wg1).ToString() << endl;
 
-    auto awg = sg1->AsWeighted(27);
-    cout << awg.ToString() << endl;
+    // auto awg = sg1->AsWeighted(27);
+    // cout << awg.ToString() << endl;
 
-    *wg1 + *sg1;
+    auto wg3 = (WeightedGraph*)graphFactory.Create("weighted", { "AB", "BC" }, { 1, 2 });
+    auto shortestPath = makeDijkstraShortestPath(*wg3, 'A', 'C');
+    cout << makeEdgesString(shortestPath) << endl;
+
+    // Errors
+    //*wg1 + *sg1;
 }
